f73395d20c01b755e515253757807b805451ee9c
Tonyfat1371 <antoniopereirasilva97@gmail.com>
2020-08-28 20:11:20 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,11 @@
 class Pessoa:
-    def __init__(self, *filhos, nome=None, idade=49):
+     olhos = 2
+
+     def __init__(self, *filhos, nome=None, idade=49):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
-    def cumprimentar(self):
+     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
@@ -19,8 +21,15 @@ if __name__ == '__main__':
         print(filho.nome)
     jose.sobrenome = 'Jerônimo Silva'
     del jose.filhos
-    print(jose.__dict__)
+    jose.olhos = 1
+    del jose.olhos
     print(toninho.__dict__)
+    print(jose.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(jose.olhos)
+    print(toninho.olhos)
+    print(id(Pessoa.olhos), id(jose.olhos), id(toninho.olhos))
 
 
 
